11553e61263472b499128d6b4547fe435d4cfeea
Paul Wachendorf <paul.wachendorf@web.de>
2018-08-06 11:44:44 +0200

    should be compatible with django 1.7, too

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['subprocess32', 'django<1.7'],
+    install_requires=['subprocess32', 'django<1.8'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
